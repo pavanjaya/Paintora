6cefe3a05304351227324b4b9c96995465cfdfc3
pavanjaya <pavanjangid.des@gmail.com>
2026-06-15 19:40:15 +0530

    Move gouache Victoria Memorial to tall left column in hero mosaic

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 import Img from './Img'
 
 const MOSAIC = [
+  { src: '/paintings/painting-3.png', alt: 'Victoria Memorial gouache' },
   { src: '/paintings/painting-1.png', alt: 'Tagore portrait' },
   { src: '/paintings/painting-2.jpg', alt: 'Victoria Memorial watercolor' },
-  { src: '/paintings/painting-3.png', alt: 'Victoria Memorial gouache' },
 ]
 
 export default function Hero() {
